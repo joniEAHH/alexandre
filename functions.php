97a0b2eb7b38c755ea18a8760394df20078b8a19
Joni <joni@bondlayer.com>
2020-04-02 22:54:08 +0100

    add woocommerce template files

--- functions.php
+++ functions.php
@@ -71,5 +71,8 @@ function register_my_menu() {
 }
 
 
-
-?>
+// To change add to cart text on single product page
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
+function woocommerce_custom_single_add_to_cart_text() {
+    return __( 'Adicionar ao carrinho', 'woocommerce' ); 
+}
