dfdc713d3f79cf7b4f13abbda5e0fbee2137cca6
Joni <joni@bondlayer.com>
2020-04-02 23:45:48 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -66,6 +66,8 @@ add_shortcode( 'timeslot_func', 'timeslot_func' );
 remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 25 );
 
 
@@ -85,4 +87,9 @@ add_filter( 'woocommerce_sale_flash', 'wc_custom_replace_sale_text' );
 function wc_custom_replace_sale_text( $html ) {
 return str_replace( __( 'Sale!', 'woocommerce' ), __( 'Promoção', 'woocommerce' ), $html );
 }
+
+function woocommerce_template_product_description() {
+  woocommerce_get_template( 'single-product/tabs/description.php' );
+}
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_product_description', 20 );
 
