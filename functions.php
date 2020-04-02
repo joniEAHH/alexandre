4fc002edf09eddc9883045a115a82175298314b9
Joni <joni@bondlayer.com>
2020-04-03 00:04:41 +0100

    remove description

--- functions.php
+++ functions.php
@@ -68,6 +68,7 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 
+
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 25 );
 
 
