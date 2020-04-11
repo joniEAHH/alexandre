e2289e52b6f2913228a85b58433f28b6e37efc01
Joni <joni@bondlayer.com>
2020-04-11 17:16:24 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -95,6 +95,24 @@ function woocommerce_template_product_description() {
 add_action( 'woocommerce_after_single_product_summary', 'woocommerce_template_product_description', 10 );
 
 
+/**
+* @snippet       Display FREE if Price Zero or Empty - WooCommerce Single Product
+* @how-to        Get CustomizeWoo.com FREE
+* @author        Rodolfo Melogli
+* @testedwith    WooCommerce 3.8
+* @donate $9     https://businessbloomer.com/bloomer-armada/
+*/
+  
+add_filter( 'woocommerce_get_price_html', 'bbloomer_price_free_zero_empty', 9999, 2 );
+   
+function bbloomer_price_free_zero_empty( $price, $product ){
+    if ( '' === $product->get_price() || 0 == $product->get_price() ) {
+        $price = '<span class="woocommerce-Price-amount amount">Grátis</span>';
+    }  
+    return $price;
+}
+
+
 
 // Remove the product description Title
 add_filter( 'woocommerce_product_description_heading', '__return_null' );
