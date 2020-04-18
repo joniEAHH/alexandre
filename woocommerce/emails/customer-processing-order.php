fa2809721138cab9c2342c4f709492dee18bc2bd
Joni <joni@bondlayer.com>
2020-04-18 19:19:41 +0100

    Update customer-processing-order.php

--- woocommerce/emails/customer-processing-order.php
+++ woocommerce/emails/customer-processing-order.php
@@ -30,24 +30,24 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 <?php
 
-/*
- * @hooked WC_Emails::order_details() Shows the order details table.
- * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
- * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
- * @since 2.5.0
- */
-do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+// /*
+//  * @hooked WC_Emails::order_details() Shows the order details table.
+//  * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
+//  * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
+//  * @since 2.5.0
+//  */
+// do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
 
-/*
- * @hooked WC_Emails::order_meta() Shows order meta data.
- */
-do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+// /*
+//  * @hooked WC_Emails::order_meta() Shows order meta data.
+//  */
+// do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
 
-/*
- * @hooked WC_Emails::customer_details() Shows customer details
- * @hooked WC_Emails::email_address() Shows email address
- */
-do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+// /*
+//  * @hooked WC_Emails::customer_details() Shows customer details
+//  * @hooked WC_Emails::email_address() Shows email address
+//  */
+// do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
 
 /**
  * Show user-defined additional content - this is set in each email's settings.
@@ -59,75 +59,87 @@ if ( $additional_content ) {
 ?>
 
 <tr>
-	<td align="center" valign="top">
-		<!-- Footer -->
-		<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+	<td valign="top" id="body_content">
+		<!-- Content -->
+		<table border="0" cellpadding="20" cellspacing="0" width="100%">
 			<tr>
-				<td align="right" valign="top">
-					<!-- Header -->
-					<p>
-						Obrigado pela sua aquisição.<br>
-						Thank you for your purchase.
-					</p>
+				<td valign="top">
+					<div id="body_content_inner">
+						<tr>
+							<td align="center" valign="top">
+								<!-- Footer -->
+								<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+									<tr>
+										<td align="right" valign="top">
+											<!-- Header -->
+											<p>
+												Obrigado pela sua aquisição.<br>
+												Thank you for your purchase.
+											</p>
 
-					<p>
-						Até breve,<br>
-						See you later,
-					</p>
-					<p style="font-weight: bold; color: black;">Alexandre Coxo</p>
-					<!-- End Header -->
-				</td>
-			</tr>
-		</table>
-		<!-- End Footer -->
-	</td>
-</tr>
+											<p>
+												Até breve,<br>
+												See you later,
+											</p>
+											<p style="font-weight: bold; color: black;">Alexandre Coxo</p>
+											<!-- End Header -->
+										</td>
+									</tr>
+								</table>
+								<!-- End Footer -->
+							</td>
+						</tr>
 
-<tr>
-	<td align="center" valign="top">
-		<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_header">
-			<tr>
-				<td align="center" style="padding: 10px;">
-					<div style="display: inline-flex;">
-						<a style="width: 20px; height: 20px; background-image: url(https://alexandrecoxo.com/wp-content/uploads/2020/04/insta-1.png); display: block; background-size: contain; background-repeat: no-repeat;" 
-						href="https://www.instagram.com/alexandre_coxo/"></a>
-						<a style="width: 20px; height: 20px; margin-left: 10px; background-image: url(https://alexandrecoxo.com/wp-content/uploads/2020/04/face-1.png); display: block; background-size: contain; background-repeat: no-repeat;" 
-						href="https://www.facebook.com/alexandrecoxoart/"></a>
+						<tr>
+							<td align="center" valign="top">
+								<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_header">
+									<tr>
+										<td align="center" style="padding: 10px;">
+											<div style="display: inline-flex;">
+												<a style="width: 20px; height: 20px; background-image: url(https://alexandrecoxo.com/wp-content/uploads/2020/04/insta-1.png); display: block; background-size: contain; background-repeat: no-repeat;" 
+												href="https://www.instagram.com/alexandre_coxo/"></a>
+												<a style="width: 20px; height: 20px; margin-left: 10px; background-image: url(https://alexandrecoxo.com/wp-content/uploads/2020/04/face-1.png); display: block; background-size: contain; background-repeat: no-repeat;" 
+												href="https://www.facebook.com/alexandrecoxoart/"></a>
+											</div>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+
+
+						<tr>
+							<td valign="top">
+								<!-- Footer -->
+								<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer_notas">
+									<tr>
+										<td valign="top">
+											<!-- Header -->
+											<p>
+											Nota:
+												Para as encomendas em Portugal o nosso operador logístico fornece o serviço de Track&Trace até ao momento da entrega. Para os restantes países, este serviço apenas é garantido até à saída de Portugal, salvo exceções. Dependendo da sua localização, a entrega demora normalmente entre 2 a 10 dias úteis a contar da data de envio. Para mais informações consulte as nossas <a href="https://alexandrecoxo.com/ajuda/">FAQ’s</a>.
+												Para mais informações contate o nosso operador logístico (<a href="https://www.ctt.pt/particulares/">aqui</a>).
+											</p>
+
+											<p>
+											Note:
+											For orders in Portugal our logistics operator provides Track & Trace service until delivery. For all other countries, this service is only guaranteed until you leave Portugal, except for exceptions. Depending on your location, shipping typically takes 2-10 business days from the date of shipment. For more information see our <a href="https://alexandrecoxo.com/ajuda/">FAQ’s</a>.
+											For more information contact our logistics operator (<a href="https://www.ctt.pt/particulares/">here</a>).
+
+											</p>
+											<!-- End Header -->
+										</td>
+									</tr>
+								</table>
+								<!-- End Footer -->
+							</td>
+						</tr>
 					</div>
 				</td>
 			</tr>
-		</table>
+		</table>		
 	</td>
-</tr>
-
-
-<tr>
-	<td valign="top">
-		<!-- Footer -->
-		<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer_notas">
-			<tr>
-				<td valign="top">
-					<!-- Header -->
-					<p>
-					Nota:
-						Para as encomendas em Portugal o nosso operador logístico fornece o serviço de Track&Trace até ao momento da entrega. Para os restantes países, este serviço apenas é garantido até à saída de Portugal, salvo exceções. Dependendo da sua localização, a entrega demora normalmente entre 2 a 10 dias úteis a contar da data de envio. Para mais informações consulte as nossas <a href="https://alexandrecoxo.com/ajuda/">FAQ’s</a>.
-						Para mais informações contate o nosso operador logístico (<a href="https://www.ctt.pt/particulares/">aqui</a>).
-					</p>
-
-					<p>
-					Note:
-					For orders in Portugal our logistics operator provides Track & Trace service until delivery. For all other countries, this service is only guaranteed until you leave Portugal, except for exceptions. Depending on your location, shipping typically takes 2-10 business days from the date of shipment. For more information see our <a href="https://alexandrecoxo.com/ajuda/">FAQ’s</a>.
-					For more information contact our logistics operator (<a href="https://www.ctt.pt/particulares/">here</a>).
-
-					</p>
-					<!-- End Header -->
-				</td>
-			</tr>
-		</table>
-		<!-- End Footer -->
-	</td>
-</tr>
-
+</tr>	
 <?php
 
 /*
